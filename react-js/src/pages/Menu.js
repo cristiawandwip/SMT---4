632dd5dbad6514f33c4a08e971098e5c1eb83429
cristiawandwip <subaza16@gmail.com>
2021-02-24 18:56:30 +0700

    Minggu - 8
    Codingan React Minggu - 8

--- react-js/src/pages/Menu.js
+++ react-js/src/pages/Menu.js
@@ -9,7 +9,7 @@ function Menu() {
             {idmenu:29, idkategori:4, menu:"Pisang Raja", gambar:"pisang.jpg",harga:4000,},
             {idmenu:34, idkategori:1, menu:"Nasi Padang", gambar:"nasipadang.jpg",harga:20000,},
             {idmenu:30, idkategori:1, menu:"Nasi Ayam", gambar:"nasiayam.jpg",harga:10000,},
-            {idmenu:6 , idkategori:2, menu:"Es Jeruk", gambar:"esjeruk.jpg",harga:3000,},
+            {idmenu:6 , idkategori:2, menu:"Es Jeruk Manis", gambar:"esjeruk.jpg",harga:3000,},
             {idmenu:9, idkategori:2, menu:"Es Teh ", gambar:"esteh.jpg",harga:2000,},
         ]
     );
